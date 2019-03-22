247e2f2d90569fa694a3dc85fe22792e1f13e04f
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-03-22 10:37:56 -0700

    Project section routers complete

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -18,4 +18,42 @@ router.get("/:id", (req, res) => {
     .catch(err => res.status(404).json(err));
 });
 
+//------ Get specific project actions------//
+router.get("/actions/:id", (req, res) => {
+  Project.getProjectActions(req.params.id)
+    .then(actions => res.status(200).json(actions))
+    .catch(err => res.status(500).json(err));
+});
+
+//--------Post New Project-----//
+router.post("/", (req, res) => {
+  Project.insert(req.body)
+    .then(newRes => res.status(201).json(newRes))
+    .catch(err => res.status(500).json(err));
+});
+
+//------Update Project w/ specific ID------//
+router.put("/:id", (req, res) => {
+  Project.update(req.params.id, req.body)
+    .then(newRes => {
+      if (newRes) {
+        res.status(201).json(newRes);
+      } else {
+        res
+          .status(400)
+          .json({
+            errMessage: "the project you tried to update doesn't exist"
+          });
+      }
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+//-------- Delete Project w/ specific ID------//
+router.delete("/:id", (req, res) => {
+  Project.remove(req.params.id)
+    .then(r => res.status(200).json({ message: "Success deleting Project" }))
+    .catch(err => res.status(500).json(err));
+});
+
 module.exports = router;
